9b41c0beaf5a950c65a265dd5289d5fe52b0b3cc
vansta <50570689+vansta@users.noreply.github.com>
2019-05-16 15:38:46 +0200

    dropdown volledig

--- dropdown.js
+++ dropdown.js
@@ -1,45 +1,83 @@
+var rooturl = "https://scrumserver.tenobe.org/scrum/api";
 
-window.onload = function () {
-    let LoginBtn = document.getElementById('loginbtn')
-    LoginBtn.addEventListener('click', fnLogin())
+        function changeURL(sNewRoot){
+            rooturl = sNewRoot;
+            console.log('root set to : ' + rooturl)
 }
 
-/*window.onclick = function(event){
-	if (!event.target.matches(
-}*/
+window.onload = function () {
+    //localstorage controle + instellen
+    if (localStorage.nickname){
+        document.getElementById("frmNickname").value = localStorage.nickname;
+    }
+    else{
+        document.getElementById('dropbtn').addEventListener('click', fnLocalStorage);
+    }
+
+    document.getElementById('frmNickname').addEventListener('click', function(e){
+        document.getElementById('frmNickname').focus();
+    })
+}
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function fnDropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
-    document.body.classList.toggle("blur");
+    document.querySelector('.main').classList.toggle("blur");
+    document.getElementById("frmNickname").focus();
+
+    window.addEventListener('keyup', function(event){
+        if (event.keyCode === 13){
+            event.preventDefault;
+            fnLogin();
+        }
+    })
+}
+
+window.onclick = function(event){
+    fnNoDropDown(event);
 }
 
 function fnLogin() {
-    let Naam = document.getElementById("frmNickname").nodeValue;
-    let Wachtwoord = document.getElementById("frmWachtwoord").nodeValue;
+    let nickname = document.getElementById("frmNickname").value;
+    let wachtwoord = document.getElementById("frmWachtwoord").value;
 
-    console.log(Naam);
-    console.log(Wachtwoord);
-
-    let url = 'https://scrumserver.tenobe.org/scrum/api/profiel/authenticate.php';
+    let url=rooturl+'/profiel/authenticate.php';
+    //let url = 'http://scrumserver.tenobe.org/scrum/api/profiel/authenticate.php';
     let data = {
-        "nickname": Naam,
-        "wachtwoord": Wachtwoord
+        nickname : nickname,
+        wachtwoord : wachtwoord
     }
 
     let request = new Request(url, {
         method: 'POST',
         body: JSON.stringify(data),
-        headers: new Headers({ 'Content-Type': 'application / json' })
+        headers: new Headers({ 'Content-Type': 'application/json' })
     });
 
     fetch(request)
         .then(function (response) { return response.json(); })
-        .then(function (data) { console.log(data); })
+        .then(function (data) { 
+            if (data.message == "Authorized"){
+                //id doorgeven aan verdere pagina's
+                sessionStorage.id = data.id;
+                //volgende window openen. vervang 'test' door correcte url
+                window.open("zoeken.html" , "_self");
+            }
+            else if (data.message == "Unauthorized"){
+                alert("nickname of wachtwoord is foutief");
+                }
+            })
         .catch(function (error) { console.log(error); });
 }
+//dropdown sluiten indien ernaast geklikt wordt
 
-window.onclick = function (event) {
+document.getElementById('index').onclick = function(event){
+    console.log(event.target);
+    fnNoDropDown(event);
+}
+//Dropdown sluiten bij naast klikken
+function fnNoDropDown (event){
     if ((!event.target.matches('.dropbtn'))) {
         if ((!event.target.matches('.dropdown-con'))) {
             var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -50,7 +88,19 @@ window.onclick = function (event) {
                     openDropdown.classList.remove('show');
                 }
             }
-            document.body.style.backgroundColor = "white";
+            document.querySelector('.main').classList.remove("blur");
+            window.removeEventListener('keyup', function(event){
+                if (event.keyCode === 13){
+                    event.preventDefault;
+                    fnLogin();
+                }
+            }); 
         }
     }
-} 
+}
+
+//nickname opslaan in local storage
+function fnLocalStorage(){
+    localStorage.nickname = document.getElementById('frmNickname').value;
+}
+
