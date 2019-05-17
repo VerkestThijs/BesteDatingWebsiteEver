fe557a1dfe6ef375882afd624f6fff7d7ca2d234
nicholasvdberghe <48473019+nicholasvdberghe@users.noreply.github.com>
2019-05-17 15:43:05 +0200

    Add files via upload

--- profiel.js
+++ profiel.js
@@ -21,9 +21,10 @@ window.onload = function () {
         .catch(function (error) { console.log(error); });
 
     //event listener: update profiel
-    document.getElementById("VerzendKnop").addEventListener("click", Updaten)
+    document.getElementById("VerzendKnop").addEventListener("click", Updaten);
 
-    //fetch data uit profielId + vul ze in als placeholder
+    //event listener: delete
+    document.getElementById("Deleteknop").addEventListener("click", Delete);
 }
 
 function InsertDataAsPlaceholder(data, velden) {
@@ -42,7 +43,7 @@ function InsertDataAsPlaceholder(data, velden) {
     velden[10].placeholder = data.grootte;
     velden[11].placeholder = data.gewicht;
     velden[12].placeholder = verbergWachtwoord(data.wachtwoord);
-    //velden[12].placeholder = data.xxx;                // functie Thijs datumNAarSterrenbeeld
+    velden[12].placeholder = creerSterrenbeeld();
 }
 
 function ToonGeslacht(geslacht) {
@@ -70,8 +71,8 @@ function verbergWachtwoord(wachtwoord) {
 
 function Updaten() {
 
-    let profielId = sessionStorage.id;   
-    //let profielId = 5000;
+    //let profielId = sessionStorage.id;   
+    let profielId = 5000;
     let url = rooturl + '/profiel/read_one.php?id=' + profielId;
 
     fetch(url)
@@ -93,8 +94,6 @@ function Updaten() {
             let nieuwWachtwoord = document.getElementById('Wachtwoord').value;
             var aIngevuldeVelden = [nieuweEmail, nieuweFoto, nieuwBeroep, nieuweHaarkleur, nieuweOogkleur, nieuweGrootte, nieuwGewicht, nieuwWachtwoord];
 
-
-
             aVeldIds = ['email', 'foto', 'beroep', 'haarkleur', 'oogkleur', 'grootte', 'gewicht', 'wachtwoord'];
 
             for (let i = 0; i < aIngevuldeVelden.length; i++) {
@@ -105,56 +104,7 @@ function Updaten() {
                 }
             }
 
-            var request = new Request(urlUpdate, {
-                method: 'PUT',
-                body: JSON.stringify(userGegevens),
-                headers: new Headers({
-                    'Content-Type': 'application/json'
-                })
-            });
-            fetch(request)
-                .then(function (resp) { return resp.json(); })
-                .then(function (data) { console.log(data); })
-                .catch(function (error) { console.log(error); });
-
-
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-}
-
-
-
-
-
-
-
-/*
-
-
-
-    fetch(url)
-        .then(function (resp) { return resp.json(); })
-        .then(function (userGegevens) {
-            //nadat de vorige promise opgelost werd kwamen we in deze procedure tercht
-            //hier kunnen we nu , met het resultat (data) van de vorige promise, aan de slag
-            //we passen de voornaam aan en sturen ook dit terug zodat deze promise afgesloten kan worden
-            let urlUpdate = rooturl + '/profiel/update.php';
-
-            aVeldIds = ['email', 'foto', 'beroep', 'haarkleur', 'oogkleur', 'grootte', 'gewicht', 'wachtwoord'];
-
-            for (let i = 0; i < aIngevuldeVelden.length; i++) {
-                if (aIngevuldeVelden[i] != "") {
-                    userGegevens[i] = aIngevuldeVelden[i];
-                    console.log(userGegevens[i]);
-                }
-                else {
-                    userGegevens[i] = userGegevens[i];
-                }
-            }
+            
 
             var request = new Request(urlUpdate, {
                 method: 'PUT',
@@ -163,46 +113,109 @@ function Updaten() {
                     'Content-Type': 'application/json'
                 })
             });
-
-            fetch(request)
-                .then(function (resp) { return resp.json(); })
-                .then(function (boodschap) {
-                    console.log(profielId);
-                    console.log(boodschap);
-                })
-                .catch(function (error) { console.log(error); });
 
             document.getElementById("profielForm").reset();
 
+            fetch(request)
+                .then(function (resp) { return resp.json(); })
+                .then(function (data) { console.log(data);
+                    let eProfielvelden = document.getElementsByClassName("inputveld");
+                    InsertDataAsPlaceholder(userGegevens, eProfielvelden);
+                 })
+                .catch(function (error) { console.log(error); });
         })
         .catch(function (error) {
             console.log(error);
         });
-
-    //placeholders terug opvullen
-    fetch(url)
-        .then(function (resp) { return resp.json(); })
-        .then(function (userGegevens) {
-            console.log(userGegevens);
-            let eProfielvelden = document.getElementsByClassName("inputveld");
-            InsertDataAsPlaceholder(userGegevens, eProfielvelden);
-        })
-        .catch(function (error) { console.log(error); });
-
-
 }
 
+/*function creerSterrenbeeld() {
 
-//validatie lege spaties
-function validatie(aIngevuldeVelden) {
-        if (aIngevuldeVelden[i] == "") {
-            console.log("leeg veld");
-            return false;
-        }
-        console.log("vontrole1");
+    let datum = document.getElementById("Geboortedatum").value;
+    let dag = datum.substr(8,2);
+    let maand = datum.substr(5,2);
+
+    let Sterrenbeeld = document.getElementById("Sterrenbeeld") 
+    console.log(datum);
+
+
+
+
+    if ((dag >= 22 && maand== 12)||(dag <=19 && maand ==01)) {
+        Sterrenbeeld.value ="Steenbok";
+        console.log(Sterrenbeeld);
+        
+    }
+    if (((dag >=20 && maand==01)||(dag <=19 && maand==02))) {
+        Sterrenbeeld.value ="Waterman";
+    
+    }
+    if (((dag >=20 && maand==02)||(dag <=20 && maand==03))){
+        Sterrenbeeld.value="Vissen";
+    }
+    if (((dag >=21 && maand==03)||(dag <=20 && maand==04))) {
+        Sterrenbeeld.value="Ram";
+    }
+    if (((dag >=21 && maand==04)||(dag <=20 && maand==05))) {
+        Sterrenbeeld.value="Stier";
+    }
+    if (((dag >=21 && maand==05)||(dag <=21 && maand==06))) {
+        Sterrenbeeld.value="Tweeling";
+    }
+    if (((dag >=21 && maand==06)||(dag <=22 && maand==07))) {
+        Sterrenbeeld.value="Kreeft";
+    }
+    if (((dag >=23 && maand==07)||(dag <=23 && maand==08))) {
+        Sterrenbeeld.value="Leeuw";
+    }
+    if (((dag >=24 && maand==08)||(dag <=23 && maand==09))) {
+        Sterrenbeeld.value="Maagd";
+    }
+    if (((dag >=24 && maand==09)||(dag <=23 && maand==10))) {
+        Sterrenbeeld.value="Weegschaal";
+        
+    }
+    if (((dag >=24 && maand==10)||(dag <=22 && maand==11))) {
+        Sterrenbeeld.value="Schorpioen";
 
     }
+    if (((dag >=23 && maand==11)||(dag <=21 && maand==12))) {
+        Sterrenbeeld.value="Boogschutter";
+    }
+        
+    
 
-    return true;
+    console.log(dag+"dag")
+    console.log(maand +"maand");
+
+}*/
+
+function Delete(){
+
+
+    var userid = sessionStorage.id;
+
+    
+
+    let url = rooturl + '/profiel/delete.php';
+    //LET OP : rooturl = https://scrumserver.tenobe.org/scrum/api
+    let data = {
+        id: userid
+    }
+
+    var request = new Request(url, {
+        method: 'DELETE',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+
+    fetch(request)
+        .then(function (resp) { return resp.json(); })
+        .then(function (data) { console.log(data); })
+        .catch(function (error) { console.log(error); });
+
 }
-*/
+
+
