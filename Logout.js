416a48cdb6059de241a4dda3d2f7f65d3ec3d4e8
rubenCaen <47660427+rubenCaen@users.noreply.github.com>
2019-05-17 13:02:48 +0200

    Delete Logout.js

--- Logout.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    let userId = sessionStorage.id;
-    document.getElementById("testInput").value = userId;
-}
-function fnLogOut(){
-    sessionStorage.clear();
-    window.open("DropDown.html","_self");
-}
